0ab067c646194eb1bdade0ad0e0baf48420efe16
ellie <elliehachemjob@gmail.com>
2022-01-21 16:29:28 +0200

    done with page need to add some logic

--- src/pages/miniPages/applicationFlowPage/flowListModalPage/flowListModal.mini.page.jsx
+++ src/pages/miniPages/applicationFlowPage/flowListModalPage/flowListModal.mini.page.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import './flowListModal.mini.styles.css';
 import ModalUsage from '../../../../components/modalUsageComponent/ModalUsage.component';
+import Button from '../../../../components/FlexibleButtonComponent/FlexibleButton.component';
 
 function FlowListModal() {
-  const [createFlows, setCreatedFlows] = useState([{ id: 1, flowName: '' }]);
+  const [createFlows, setCreatedFlows] = useState([
+    { id: 1, flowName: 'Main Registration  Flow' },
+    { id: 2, flowName: 'USA  Registration flow' },
+    { id: 3, flowName: 'UK  Registration Flow' },
+  ]);
 
   return (
     <ModalUsage
@@ -19,7 +24,28 @@ function FlowListModal() {
       }}
       modalBackground='#C1E0F7'
     >
-      Choose Flow
+      {createFlows.map((item) => {
+        return (
+          <div className='wrapper-flowListModal'>
+            <div key={item.key}>
+              <Button
+                className='button-fieldsAdd'
+                name={item.flowName}
+                variant='contained'
+                sx={{
+                  backgroundColor: '#2B3A67',
+                  borderRadius: '10px',
+                  fontWeight: 600,
+                  '&:hover': {
+                    backgroundColor: '#5E807F',
+                    borderRadius: '10px',
+                  },
+                }}
+              />
+            </div>
+          </div>
+        );
+      })}
     </ModalUsage>
   );
 }
--- src/pages/miniPages/flowConfigurationPage/flowConfiguration.mini.page.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './flowConfiguration.mini.styles.css';
-
-function FlowConfiguration() {
-  return <div></div>;
-}
-
-export default FlowConfiguration;
