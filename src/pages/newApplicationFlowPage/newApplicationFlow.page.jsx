4044223e768ba3c94b1373b68bbb7b2433ecfc21
ellie <elliehachemjob@gmail.com>
2022-02-04 16:02:00 +0200

    structure  of flow

---
+++ src/pages/newApplicationFlowPage/newApplicationFlow.page.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './newApplicationFlow.styles.css';
+function NewApplicationFlow() {
+  return (
+    <>
+      {/* 1st left side */}
+      <div>Background color </div>
+      <div>Go Back Icon</div>
+      <div>Flow Setup Header</div>
+      <div>Levels Label</div>
+      {/* 2nd */}
+      <div>box1</div>
+      <div>box1Icon</div>
+      <div>box2</div>
+      <div>box2Icon</div>
+      <div>box3</div>
+      <div>box3Icon</div>
+      <div>box4</div>
+      <div>box4Icon</div>
+      <div>box5</div>
+      <div>box5Icon</div>
+      <div>box6</div>
+      <div>box6Icon</div>
+      <div>box7</div>
+      <div>box7Icon</div>
+      <div>box8</div>
+      <div>box8Icon</div>
+      <div>box9</div>
+      <div>box9Icon</div>
+      <div>box10</div>
+      <div>box10Icon</div>
+      <div>box11</div>
+      <div>box11Icon</div>
+      <div>box12</div>
+      <div>box12Icon</div>
+      <div>Submit Flow Button</div>
+      {/* 3rd right side */}
+      <div>Background color </div>
+      <div>box1</div>
+      <div>Divider</div>
+      <div>box2</div>
+      <div>Divider</div>
+      <div>box3</div>
+      {/* 4th  later deduced*/}
+    </>
+  );
+}
+
+export default NewApplicationFlow;
